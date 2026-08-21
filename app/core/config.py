a96f2a5b36d9c1c2160d543b72bad20bc4269a7c
Morshed07 <inayeem729@gmail.com>
2026-08-21 17:47:28 +0600

    Update project name to FastAPI Auth System

--- app/core/config.py
+++ app/core/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "Ecommerce Backend API"
+    PROJECT_NAME: str = "FastAPI Auth System"
     VERSION: str = "1.0.0"
     API_V1_STR: str = "/api/v1"
     DEBUG: bool = True
